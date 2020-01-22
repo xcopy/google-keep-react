7d43d66ee877247df7a21b548faf24d7528d47f0
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-01-22 21:14:27 +0600

    Add react-fontawesome, add some icons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import {Navbar, Container} from 'react-bootstrap';
+import {Nav, Navbar, Container} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCog, faRedo} from '@fortawesome/free-solid-svg-icons';
 
 class Layout extends React.Component {
     render() {
         return (
             <div>
-                <Navbar bg="light">
+                <Navbar bg="light justify-content-between">
                     <Navbar.Brand href="#">
                         <div className="d-flex align-items-center">
                             <img src="/logo-40x40.png" width="40" height="40" alt=""/>
                             <h4 className="d-inline-block ml-2 my-0 text-muted">Keep</h4>
                         </div>
                     </Navbar.Brand>
+                    <Nav>
+                        <Nav.Link href="#">
+                            <FontAwesomeIcon icon={faRedo} size="lg"/>
+                        </Nav.Link>
+                        <Nav.Link href="#">
+                            <FontAwesomeIcon icon={faCog} size="lg"/>
+                        </Nav.Link>
+                    </Nav>
                 </Navbar>
                 <Container/>
             </div>
