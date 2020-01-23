33f8636153c65098a437c0692b3c9dc463ac9fe4
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-01-23 22:26:03 +0600

    Add note (basic modal & form)

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,20 +1,81 @@
 import React, {Component} from 'react';
 import Note from './Note';
-import {Row, Col, Button} from 'react-bootstrap';
+import {Row, Col, Button, Modal, Form} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faLightbulb} from '@fortawesome/free-regular-svg-icons';
+
+const initialState = {
+    modalShown: false,
+    formValidated: false,
+    note: {
+        title: '',
+        content: ''
+    }
+};
 
 class NoteList extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            notes: []
+            notes: [],
+            ...initialState
         };
+
+        this.showModal = this.showModal.bind(this);
+        this.hideModal = this.hideModal.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    showModal() {
+        this.setState({
+            modalShown: true
+        });
+    }
+
+    hideModal() {
+        this.setState({
+            modalShown: false,
+            formValidated: false
+        });
+    }
+
+    handleChange(e) {
+        e.persist();
+
+        this.setState(state => {
+            let note = Object.assign({}, state.note);
+
+            note[e.target.name] = e.target.value;
+
+            return {note};
+        });
+    }
+
+    handleSubmit(e) {
+        const form = e.currentTarget;
+
+        e.preventDefault();
+
+        if (!form.checkValidity()) {
+            e.stopPropagation();
+        } else {
+            this.setState(state => {
+                return {
+                    notes: [state.note, ...state.notes],
+                    ...initialState
+                };
+            });
+        }
+
+        this.setState({
+            formValidated: true
+        });
     }
 
     render() {
-        const {notes} = this.state;
+        const {notes, modalShown, formValidated, note} = this.state;
         const template = notes.map((note, i) =>
             <Col lg={3} key={i.toString()}>
                 <Note note={note}/>
@@ -31,9 +92,41 @@ class NoteList extends Component {
                     <div className="text-black-50 text-center">
                         <FontAwesomeIcon icon={faLightbulb} size="6x"/>
                         <h3 className="m-3">Notes you add appear here</h3>
-                        <Button variant="outline-secondary">Add new one</Button>
+                        <Button variant="outline-secondary" onClick={this.showModal}>Add new one</Button>
                     </div>
                 )}
+
+                <Modal show={modalShown} centered onHide={this.hideModal}>
+                    <Modal.Header>
+                        <Modal.Title>Take a note</Modal.Title>
+                    </Modal.Header>
+                    <Form noValidate validated={formValidated} onSubmit={this.handleSubmit}>
+                        <Modal.Body>
+                                <Form.Group controlId="note-title">
+                                    <Form.Control
+                                        type="text"
+                                        name="title"
+                                        placeholder="Title"
+                                        value={note.title}
+                                        onChange={this.handleChange}/>
+                                </Form.Group>
+                                <Form.Group controlId="note-content" className="mb-0">
+                                    <Form.Control
+                                        as="textarea"
+                                        name="content"
+                                        rows="3"
+                                        placeholder="Content"
+                                        value={note.content}
+                                        onChange={this.handleChange}
+                                        required/>
+                                </Form.Group>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={this.hideModal}>Cancel</Button>
+                            <Button variant="primary" type="submit">Save</Button>
+                        </Modal.Footer>
+                    </Form>
+                </Modal>
             </div>
         );
     }
