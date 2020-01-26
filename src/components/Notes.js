1f4938a715274d6dfbb8e4484fd191892544b9ba
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-01-27 00:50:48 +0600

    Show message if no notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Note from './Note';
 import {Row, Col} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faLightbulb} from '@fortawesome/free-regular-svg-icons';
 import './Notes.scss';
 
 class Notes extends Component {
@@ -9,13 +11,20 @@ class Notes extends Component {
         const {notes, deleteNote, archiveNote} = this.props;
 
         return (
-            <Row>
-                {notes && notes.map(note =>
-                    <Col xl="3" lg="4" md="6" sm="6" key={note.id}>
-                        <Note {...note} {...{deleteNote, archiveNote}}/>
-                    </Col>
-                )}
-            </Row>
+            notes.length ? (
+                <Row>
+                    {notes.map(note =>
+                        <Col xl="3" lg="4" md="6" sm="6" key={note.id}>
+                            <Note {...note} {...{deleteNote, archiveNote}}/>
+                        </Col>
+                    )}
+                </Row>
+            ) : (
+                <div className="text-center text-muted mt-5">
+                    <FontAwesomeIcon icon={faLightbulb} size="6x"/>
+                    <h4 className="mt-3 mb-0">Your notes appear here</h4>
+                </div>
+            )
         );
     }
 }
