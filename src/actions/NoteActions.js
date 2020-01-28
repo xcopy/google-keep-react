39200c900c68108a41379b3b351b84afd62b6939
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-01-28 09:38:34 +0600

    Add new actions

--- src/actions/NoteActions.js
+++ src/actions/NoteActions.js
@@ -1,5 +1,6 @@
 export const ADD_NOTE = 'ADD_NOTE';
 export const DELETE_NOTE = 'DELETE_NOTE';
+export const DELETE_NOTE_FOREVER = 'DELETE_NOTE_FOREVER';
 export const ARCHIVE_NOTE = 'ARCHIVE_NOTE';
 
 export const addNote = note => ({
@@ -7,14 +8,16 @@ export const addNote = note => ({
     note
 });
 
-export const deleteNote = id => ({
+export const deleteNote = (id, restore = false) => ({
     type: DELETE_NOTE,
     id,
-    key: 'deleted_at'
+    key: 'deleted_at',
+    restore
 });
 
-export const archiveNote = id => ({
+export const archiveNote = (id, restore = false) => ({
     type: ARCHIVE_NOTE,
     id,
-    key: 'archived_at'
+    key: 'archived_at',
+    restore
 });
--- src/reducers/notes.js
+++ src/reducers/notes.js
@@ -1,6 +1,7 @@
 import {
     ADD_NOTE,
     DELETE_NOTE,
+    DELETE_NOTE_FOREVER,
     ARCHIVE_NOTE
 } from '../actions/NoteActions';
 import md5 from 'md5';
@@ -25,11 +26,22 @@ const notes = (state = [], action) => {
             _state = [...state];
 
             _note = _state.find(predicate);
-            _note[action.key] = new Date().toISOString();
+            _note[action.key] = action.restore ? null : new Date().toISOString();
 
             const _index = _state.findIndex(predicate);
 
             _index !== -1 && _state.splice(_index, 1, _note);
+
+            return _state;
+        }
+        case DELETE_NOTE_FOREVER: {
+            const predicate = note => note.id === action.id;
+
+            _state = [...state];
+
+            const _index = _state.findIndex(predicate);
+
+            _index !== -1 && _state.splice(_index, 1);
 
             return _state;
         }
