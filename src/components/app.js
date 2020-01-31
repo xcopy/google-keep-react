d660b675bb70a7deadfd576a4dbd6162f05b969f
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-01-31 16:10:18 +0600

    App component refactored

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Header from '../header';
-import Notes from '../notes';
+import Header from './header';
+import Notes from './notes';
 
 import 'bootstrap/scss/bootstrap.scss';
 import './app.scss';
